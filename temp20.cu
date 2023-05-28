7e2dbc56cd13be5b449b5a8d9a4e79f7871608bc
MamedovAR <116785123+MamedovAR@users.noreply.github.com>
2023-05-28 13:47:32 +0700

    Update temp20.cu

--- temp20.cu
+++ temp20.cu
@@ -2,14 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cub/cub.cuh>
-#include "cuda_graph.h"
 #include <cub/block/block_load.cuh>
 #include <cub/block/block_store.cuh>
 #include <cub/block/block_reduce.cuh>
 #define IDX2F(i,j,ld) (((j)-1)*(ld))+((i)-1)
 #define IDX2C(i,j,ld) (((j)*(ld))+(i))
-using namespace cub;
-using namespace std;
+
 __global__ void change(float* setka, float* arr, int s)
 {
 	int i = blockDim.x * blockIdx.x + threadIdx.x;
@@ -19,10 +17,13 @@ __global__ void change(float* setka, float* arr, int s)
 }
 
 __global__ void subtract_modulo_kernel(float* d_in1, float* d_in2, float* d_out, int size) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < size) {
-        int diff = d_in1[idx] - d_in2[idx];
-        d_out[idx] = fabs(diff);
+    int idx = blockDim.x * blockIdx.x + threadIdx.x;
+    if (idx < size*size) {
+        float diff = d_in1[idx] - d_in2[idx];
+        if(diff<0)
+          d_out[idx]=-diff;
+        else
+          d_out[idx]=diff;
     }
 }
 
@@ -92,47 +93,85 @@ int main(int argc, char** argv)
       
       }
     }
+    cudaSetDevice(3);
     float *cusetka;
     float *cuarr;
     float* cuarr2;
-    cudaMalloc((void**)&cusetka, s*s*sizeof(float));
-    cudaMalloc((void**)&cuarr2, s*s*sizeof(float));
-    cudaMalloc((void**)&cuarr, s*s*sizeof(float));
-    cudaMemcpy(cuarr2, cuarr2, s*s*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(cusetka, setka, s*s*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(cuarr, arr, s*s*sizeof(float), cudaMemcpyHostToDevice);
+    cudaError_t stat;
+    cudaStream_t stream;
+    void* d_temp_storage = NULL;
+    size_t temp_storage_bytes = 0;
+    float* max_value;
+    stat=cudaMalloc((void**)&cusetka, s*s*sizeof(float));
+    if(stat!=cudaSuccess)printf("err 1: %d", stat);
+    stat=cudaMalloc((void**)&cuarr2, s*s*sizeof(float));
+    if(stat!=cudaSuccess)printf("err 2: %d", stat);
+    stat=cudaMalloc((void**)&cuarr, s*s*sizeof(float));
+    if(stat!=cudaSuccess)printf("err 2: %d", stat);
+    stat=cudaMemcpy(cuarr2, arr2, s*s*sizeof(float), cudaMemcpyHostToDevice);
+    if(stat!=cudaSuccess)printf("err 3: %d", stat);
+    stat=cudaMemcpy(cusetka, setka, s*s*sizeof(float), cudaMemcpyHostToDevice);
+    if(stat!=cudaSuccess)printf("err 4: %d", stat);
+    stat=cudaMemcpy(cuarr, arr, s*s*sizeof(float), cudaMemcpyHostToDevice);
+    if(stat!=cudaSuccess)printf("err 5: %d", stat);
+    stat=cudaMalloc((void**)&max_value, sizeof(float));
+    if(stat!=cudaSuccess)printf("err 6: %d", stat);
+    stat=cub::DeviceReduce::Max(d_temp_storage, temp_storage_bytes, cuarr2, max_value, s*s);
+    if(stat!=cudaSuccess)printf("err 7: %d", stat);
+    stat=cudaMalloc(&d_temp_storage,temp_storage_bytes);
+    if(stat!=cudaSuccess)printf("err 8: %d", stat);
+    float* max_value_h=(float*)malloc(sizeof(float));
     while(err>a && iter<n)
     {
       iter++;
       if(iter%100==1)
         err=0;
         //Этого должно хватить для вычисления массива.
-      change<<<n, n, 0 >>>(cusetka, cuarr, n);
+      cudaGraph_t graph;
+      cudaGraphExec_t instance;
+//      cudaStreamBeginCapture(stream,cudaStreamCaptureModeGlobal);
+      change<<<s, s, 0 >>>(cusetka, cuarr, s);
+//      cudaStreamEndCapture(stream, &graph);
+//      cudaGraphInstantiate(&instance,graph,NULL,NULL,0);
 //      change<<<blocksPerGrid, threadsPerBlock >>>(cuarr, cusetka, n);
       if(iter%100==1)
       {
-        const int block_size = 256;
-        const int num_blocks = (n + block_size - 1) / block_size;
-        subtract_modulo_kernel<<<num_blocks, block_size>>>(cusetka, cuarr, arr2, n);
+        subtract_modulo_kernel<<<s, s, 0>>>(cusetka, cuarr, cuarr2, s);
+        // cudaMemcpy(setka,cuarr2,s*s*sizeof(float),cudaMemcpyDeviceToHost);
+        // if(s<16)
+        // {
+        //   for(int i=0; i<s; i++)
+        //   {
+        //     for(int j=0; j<s; j++)
+        //       printf("%f ",setka[i+s*j]);
+        //     printf("\n");
+          
+        //   }
+        // }
+        // const int block_size = 256;
+        // const int num_blocks = (n + block_size - 1) / block_size;
 
-        cudaDeviceSynchronize();
+//        cudaDeviceSynchronize();
 
-        int max_value;
-        void* d_temp_storage = nullptr;
-        size_t temp_storage_bytes = 0;
-        DeviceReduce::Max(d_temp_storage, temp_storage_bytes, arr2, &max_value, n);
-
-        err=max_value;
+        stat=cub::DeviceReduce::Max(d_temp_storage, temp_storage_bytes, cuarr2, max_value, s*s);
+        if(stat!=cudaSuccess)printf("%d\n",stat);
+        cudaMemcpy(max_value_h,max_value,sizeof(float),cudaMemcpyDeviceToHost);
+        err=max_value_h[0];
         printf("%d %f\n", iter, err);
       }
 
-      float* dop;
-      dop = cuarr;
-      cuarr=cusetka;
-      cusetka = dop;
+      // float* dop;
+      // dop = cuarr;
+      // cuarr=cusetka;
+      // cusetka = dop;
+      cudaMemcpy(cuarr2,cuarr,s*s*sizeof(float),cudaMemcpyDeviceToDevice);
+      cudaMemcpy(cuarr,cusetka,s*s*sizeof(float),cudaMemcpyDeviceToDevice);
+      cudaMemcpy(cusetka,cuarr2,s*s*sizeof(float),cudaMemcpyDeviceToDevice);
     }
     cudaMemcpy(setka,cusetka,s*s*sizeof(float),cudaMemcpyDeviceToHost);
     cudaMemcpy(arr, cuarr, s*s*sizeof(float), cudaMemcpyDeviceToHost);
+    free(max_value_h);
+    cudaFree(d_temp_storage);
     cudaFree(cusetka);
     cudaFree(cuarr);
     printf("Count iterations: %d\nError: %.10f\n", iter,err);
@@ -143,7 +182,6 @@ int main(int argc, char** argv)
         for(int j=0; j<s; j++)
           printf("%f ",setka[i+s*j]);
         printf("\n");
-      
       }
     }
     free(setka);
