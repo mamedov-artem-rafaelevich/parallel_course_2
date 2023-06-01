eee037fa6246164f1c6c0dc39321c78a5c6f2ac3
MamedovAR <116785123+MamedovAR@users.noreply.github.com>
2023-06-01 15:20:58 +0700

    Update writer.c

--- writer.c
+++ writer.c
@@ -6,26 +6,42 @@ int main()
         FILE* fp = fopen("start.bin","wb+");
         float array[1024];
         for(int i=0; i<1024; i++)
-                array[i]=(float)(rand()%255)/255;
-        fwrite(&array,sizeof(float),1024,fp);
+	{
+                array[i]=((float)(rand()%255))/255;
+		printf("%f ",array[i]);
+	}
+	printf("\n");
+        fwrite(array,sizeof(float),1024,fp);
         fclose(fp);
         fp = fopen("fc1.bin","wb+");
         float fc1[1024*256];
         for(int i=0; i<1024*256; i++)
-                fc1[i]=(float)(rand()%255)/255;
-        fwrite(&fc1,sizeof(float),1024*256,fp);
+	{
+                fc1[i]=((float)(rand()%255))/255;
+		printf("%f ",fc1[i]);
+	}
+	printf("\n");
+        fwrite(fc1,sizeof(float),1024*256,fp);
         fclose(fp);
         fp = fopen("fc2.bin","wb+");
         float fc2[256*16];
         for(int i=0; i<16*256; i++)
-                fc2[i]=(float)(rand()%255)/255;
-        fwrite(&fc2,sizeof(float),16*256,fp);
+	{
+                fc2[i]=((float)(rand()%255))/255;
+		printf("%f ",fc2[i]);
+	}
+	printf("\n");
+        fwrite(fc2,sizeof(float),16*256,fp);
         fclose(fp);
         fp = fopen("fc3.bin","wb+");
         float fc3[16];
         for(int i=0; i<16; i++)
-                fc3[i]=(float)(rand()%255)/255;
-        fwrite(&fc3,sizeof(float),16,fp);
+	{
+                fc3[i]=((float)(rand()%255))/255;
+		printf("%f ",fc3[i]);
+	}
+	printf("\n");
+        fwrite(fc3,sizeof(float),16,fp);
         fclose(fp);
         return 0;
 }
